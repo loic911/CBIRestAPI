99cd9d85a1ec06d9ebc0ce43740d43d8f15d52f5
Loïc Rollus <loicrollus@gmail.com>
2015-03-12 07:12:06 +0100

    remove storach sample

--- src/main/webapp/scripts/storach/service_storach.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-retrievalApp.factory('Storach', function ($resource) {
-        return $resource('app/rest/storachs/:id', {}, {
-            'query': { method: 'GET', isArray: true},
-            'get': { method: 'GET'}
-        });
-    });
